dcec12c59f2f4aed6e1fbafd5457d096fea3cc5c
Journey_Lee <rmfo2@naver.com>
2022-06-28 16:21:22 +0900

    3rd

--- server/app.js
+++ server/app.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 
 const app = express();
-const port = process.env.port
+const port = process.env.PORT || 3001;
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + "/build/index.html")
 })
 app.use(express.static('build'))
 
-app.listen(port || 3001, ()=>{
+app.listen(port, ()=>{
     console.log('on 3001')
 })
